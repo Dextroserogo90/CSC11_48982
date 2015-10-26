419623303a1483e7bfff2a39f99f768ee46e4395
Deepak Menon <Dextroserogo@yahoo.com>
2015-10-26 16:25:21 -0700

    Completed C++ first part

---
+++ Midterm/first/midterm_1/build/Debug/Cygwin_4.x-Windows/emp_pay.o.d
@@ -0,0 +1,1 @@
+build/Debug/Cygwin_4.x-Windows/emp_pay.o: emp_pay.h
--- Midterm/first/midterm_1/main.cpp
+++ Midterm/first/midterm_1/main.cpp
@@ -2,7 +2,13 @@
 #include <iostream>
 #include <stdio.h>
 
+
+
 using namespace std;
+
+void emp_pay();
+void isp();
+void fib();
 
 int main(int argc, char *argv[])
 {
@@ -10,21 +16,26 @@ int main(int argc, char *argv[])
 	unsigned int R0;
 	printf("Please select a choice from 1-4 \n"
                 "1. Employees Pay \n"
-                "2.\n"
-                "3.\n"
-         	"4 is for exit \n");
+                "2. ISP packages and fees \n"
+                "3. Fibonacci sequence \n"
+         	"4  Exit \n");
 	scanf("%d",&R0);
 	
 	
-	if(R0 == 1) goto _first;
-	//else if (R0 == 2) goto _second;
-	//else if (R0 == 3) goto _third;
-	else if (R0 == 4) goto _exit;
+	if(R0 == 1) {emp_pay();goto _menu;}
+	else if (R0 == 2) {isp();goto _menu;}
+	else if (R0 == 3) {fib();goto _menu;}
+	else if (R0 == 4) {goto _exit;}
 	else goto _menu;
 	
+        
+    //User exit
+     _exit:
+    return 0;
+}
 
+void emp_pay(){
     //First program
-    _first:
     unsigned int R1, R2, R3;
     
     //Prompt
@@ -59,13 +70,97 @@ int main(int argc, char *argv[])
      _foutput:
      printf("Gross pay: $");
      printf("%d",R3);
-     printf("\n");
-     goto _menu;
-	 
-     //User exit
-     _exit:
-    return 0;
+     printf("\n");    
 }
 
+void isp(){
+_second:
 //Second program
-//_second:
+//Customer's variables
+
+char R1;
+unsigned int R2 = 0;
+unsigned int R3 = 0;
+
+//Prompt user for the package
+printf("Enter the package you have subscribed to \n"
+"(a). $30 per month, 11 hours access.\n"
+"(b). $35 per month, 22 hours access.\n"
+"(c). $40 per month, 33 hours access.\n");
+cin>>R1;
+
+printf("Enter the number of hours watched: \n");
+scanf("%d",&R2);
+
+if(R1 == 'a'){
+    goto _package1;
+}
+else if(R1 == 'b'){
+    goto _package2;
+}
+else if (R1 == 'c'){
+    goto _package3;
+}
+else goto _second;
+
+_package1:
+R3 = 30;
+
+if(11<R2<=22){
+    R3+=(R2-11)*3;
+}
+else if(22<R2){
+    R3+=(3*11)+(R2-22)*6;
+}
+goto _output;
+
+_package2:
+R2=35;
+
+if(22<R2<=44){
+    R3+=(R2-22)*2;
+}
+else if(44<R2){
+    R3+=(22*2)+(R2-44)*4;
+}
+goto _output;
+
+_package3:
+R3=40;
+if(33<R2<=66){
+    R3+=(R2-33)*1;
+}
+else if(66<=R2){
+    R3+=(33*1)+(R2-66)*2;
+}
+goto _output;
+
+_output:
+//Outputs for monthly charges
+printf("The monthly charges with the fees: $");
+printf("%d",R3);  
+cout<<endl;
+}
+
+void fib(){
+//Declare variables
+
+unsigned int R0=0; //Input
+unsigned int R1=1; //Output at the number
+unsigned int R2=1; //Starting point
+unsigned int R3 = 0;
+
+printf("Enter the term that you would like to get the Fibonnacci sequence of: \n");
+cin>>R3;
+
+_theloop:
+R0= R1 + R2;
+R1 = R2;
+R2 = R0;
+R3--;
+if(R3!=2)goto _theloop;
+
+//output
+printf("%d",R0);
+cout<<endl;
+}
